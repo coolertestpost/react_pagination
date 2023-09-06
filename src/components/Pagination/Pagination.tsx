115ec0f1ba4ec930ae1e8b637d835fd3e19c2d9d
coolertestpost <dedinside66775@gmail.com>
2023-09-06 19:34:44 +0300

    in one step to solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,70 @@
-export const Pagination = () => {};
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-plusplus */
+import React from 'react';
+
+interface Props {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  function getTotalItems(totalElements: number, itemsPerPage: number) {
+    const groups = Math.ceil(totalElements / itemsPerPage);
+    const result = [];
+
+    for (let i = 1; i <= groups; i++) {
+      result.push(i);
+    }
+
+    return result;
+  }
+
+  const totalArray = getTotalItems(total, perPage);
+
+  console.log(currentPage);
+
+  return (
+    <ul className="pagination">
+      <li className="page-item disabled">
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled="true"
+        >
+          «
+        </a>
+      </li>
+      {totalArray.map((item) => (
+        <li
+          className={`page-item ${currentPage === item ? 'active' : ''}`}
+          onClick={() => {
+            if (currentPage !== item) {
+              onPageChange(currentPage);
+            }
+          }}
+        >
+          <a data-cy="pageLink" className="page-link" href={`#${item}`}>{item}</a>
+        </li>
+      ))}
+      <li className="page-item">
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled="false"
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
